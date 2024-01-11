4688703e8e2482337911363241b5515c4319396a
kranthi Kumar <kranthi@lightsapien.com>
2024-01-11 23:07:30 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Awesome Blog</h1>
+      <div id="page-body">
+        Welcome to my blog!
+      </div>
+    </div>
+  );
+}
+
+export default App;
