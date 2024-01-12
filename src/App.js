37d122189a68dee481f2f0654a5ec8f35ec4fe1b
kranthi kumar <g.kranthi2507@gmail.com>
2024-01-12 12:23:57 +0530

    add pages and components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from './pages/HomePage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import AboutPage from './pages/AboutPage';
+import NavBar from './NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Awesome Blog</h1>
-      <div id="page-body">
-        Welcome to my blog!
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/article/:articleId" element={<ArticlePage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,15 @@
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+
+const ArticlePage = () => {
+    const { articleId } = useParams();
+    const article = articles.find(article => article.name === articleId)
+    return (
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph, i) => (<p key={i}>{paragraph}</p>))}
+        </>
+    );
+}
+
+export default ArticlePage;
