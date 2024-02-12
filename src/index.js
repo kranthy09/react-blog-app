40ffc7ff49712f74db14285c802f693eda01da52
kranthi kumar <g.kranthi2507@gmail.com>
2024-02-12 20:58:36 +0530

    authentication

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBHnhha6c79ikWEOiYll9gtaKhc_-94zI",
+  authDomain: "my-react-blog-541c1.firebaseapp.com",
+  projectId: "my-react-blog-541c1",
+  storageBucket: "my-react-blog-541c1.appspot.com",
+  messagingSenderId: "29813682515",
+  appId: "1:29813682515:web:39120303edd1dfee0f31a6"
+};
+
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +25,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,19 +1,58 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import CommentsList from "../components/commentsList";
+import AddCommentForm from "../components/AddCommentForm";
+import useUser from "../hooks/useUser";
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] })
+
     const { articleId } = useParams();
+    const { user, isLoading } = useUser();
+    console.log(articleId);
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            console.log(response.data);
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();
+    }, []);
     const article = articles.find(article => article.name === articleId)
+
+    const addUpvote = async () => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = response.data;
+        setArticleInfo(updatedArticle);
+    }
+
+
     if (!article) {
         return (
-            <NotFoundPage/>
+            <NotFoundPage />
         )
     }
+    console.log(articleInfo);
     return (
         <>
             <h1>{article.title}</h1>
+            <div className="upvotes-section">
+                { user ?
+                <button onClick={addUpvote}>Upvote</button>
+                : <button>Login</button>}
+            </div>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
             {article.content.map((paragraph, i) => (<p key={i}>{paragraph}</p>))}
+            {user? 
+            <AddCommentForm
+                articleName={articleId}
+                onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+                : <button>Log in to add a comment</button>}
+            <CommentsList comments={articleInfo.comments} />
         </>
     );
 }
